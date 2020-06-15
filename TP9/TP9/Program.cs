ab2f3bb668465e513a765d7d78ce2b3052fac4bf
FNMariani <facundonicolasm@gmail.com>
2020-06-15 16:23:10 -0300

    TP9.1

---
+++ TP9/TP9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace TP9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribimos archivo de configuracion de destino
+            string ruta = @"C:\Taller1\tpn9-FNMariani\TP9\TP9\bin\Debug\destino\";
+            Helpers.SoporteParaConfiguracion.CrearArchivoDeConfiguracion(ruta);
+
+            //Leemos destino dentro del archivo de configuracion
+            string rutaDeLectura = Helpers.SoporteParaConfiguracion.LeerConfiguracion();
+
+            //Listamos archivos
+            string directorio = System.Environment.CurrentDirectory;
+            List<string> listadoDeArchivos = Directory.GetFiles(directorio).ToList();
+            foreach (string archivo in listadoDeArchivos)
+            {
+                Console.WriteLine(archivo);
+
+                //Si el archivo tiene extension .mp3 o .txt, lo movemos
+                FileInfo file = new FileInfo(archivo);
+                if( (file.Extension == ".mp3") || (file.Extension == ".txt") )
+                {
+                    string nuevoDestino = rutaDeLectura + file.Name;
+                    File.Move(archivo, nuevoDestino);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TP9/TP9/SoporteParaConfiguracion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    public static class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion(string ruta)
+        {
+            string nuevoArchivo = "destino.dat";
+
+            if(!File.Exists(nuevoArchivo))
+            {
+                File.Create(nuevoArchivo);
+            }
+            File.WriteAllText(nuevoArchivo, ruta);
+        }
+
+        public static string LeerConfiguracion()
+        {
+            string nuevoArchivo = "destino.dat";
+
+            string ruta = File.ReadAllText(nuevoArchivo);
+
+            return ruta;
+        }
+    }
+}
